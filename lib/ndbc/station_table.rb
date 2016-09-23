ec35e62521cb36003e4f5eb057c7d59692021c5e
Derek Kniffin <derek.kniffin@gmail.com>
2016-09-23 14:29:14 -0400

    Add some functionality to parse out the tide_station_id
    This station id is used for the tidesandcurrents.noaa.gov api

--- lib/ndbc/station_table.rb
+++ lib/ndbc/station_table.rb
@@ -18,12 +18,18 @@ module NDBC
             time_zone:  station_parts[7],
             forecast:   station_parts[8],
             note:       station_parts[9],
-            active:     active?(station_parts)
+            active:     active?(station_parts),
+            tide_station_id: tide_station_id(station_parts[4])
           }
         end
       end
 
       private
+
+      def tide_station_id(name)
+        return '' unless name
+        name.match(/\d{7}/).to_s
+      end
 
       def cleanup_parts!(station_parts)
         station_parts.map! do |part|
